2bfff8429013608a72a069f8d9ab68205ff55c0e
MrSakine <msakine20@gmail.com>
2023-12-05 17:25:12 +0000

    [added] a class BaseModel that defines all common attributes/methods for other classes

---
+++ models/base_model.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+This module is the base model class
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """
+    Class BaseModel that defines all common
+    attributes/methods for other classes
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize attributes of the class
+
+        Attributes:
+            - id (optional, str): the id of the object
+            - created_at (optional, datetime): the creation date of the object
+            - updated_at (optional, datetime): last modified date of the object
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """
+        Update the attribute @updated_at of the base model object
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Convert the base model object to dict
+
+        Returns: a dictionary consisting of
+        attribute names of the base model object as keys of the dictionary
+        and their values as values of the dictionary
+        """
+        response = {}
+        for k, v in self.__dict__.items():
+            if k == "created_at" or k == "updated_at":
+                response[k] = v.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                response[k] = v
+        response["__class__"] = self.__class__.__name__
+        return (response)
+
+    def __str__(self) -> str:
+        """
+        String representation of the object
+        """
+        return (
+            "[{0}] ({1}) {2}".format(
+                self.__class__.__name__, self.id, str(self.__dict__)
+            )
+        )
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,8 +3,92 @@
 This module is all unit tests about base model class
 """
 import unittest
+from datetime import datetime
+from models.base_model import BaseModel
 
 
-class TestBaseModel(unittest.TestCase):
+class TestBaseModelInstantiation(unittest.TestCase):
+    """Unittests for testing instantiation of the Base Model class"""
+
     def setUp(self) -> None:
-        print("Base model unit tests")
+        pass
+
+    def test_id_initialization(self):
+        bm = BaseModel()
+        self.assertIsInstance(bm.id, str)
+
+    def test_created_at_initialization(self):
+        bm = BaseModel()
+        self.assertIsInstance(bm.created_at, datetime)
+
+    def test_updated_at_initialization(self):
+        bm = BaseModel()
+        self.assertIsInstance(bm.created_at, datetime)
+
+    def test_two_objects(self):
+        bm = BaseModel()
+        bm2 = BaseModel()
+        self.assertTrue(bm.id != bm2.id)
+
+
+class TestBaseModelSaveMethod(unittest.TestCase):
+    """Unittests for testing save method of the Base Model class"""
+
+    def test_save_object(self):
+        bm = BaseModel()
+        temp = bm.updated_at
+        bm.save()
+        self.assertTrue(bm.updated_at > temp)
+
+
+class TestBaseModelToDictMethod(unittest.TestCase):
+    """Unittests for testing to_dict method of the Base Model class"""
+
+    def test_contain_known_keyword(self):
+        bm = BaseModel()
+        known_keys = ["id", "created_at", "updated_at", "__class__"]
+        for k in known_keys:
+            self.assertIsNotNone(bm.__getattribute__(k))
+
+    def test_contain_unknown_keyword(self):
+        bm = BaseModel()
+        unknown_keys = ["i", "created_a", "updated_a", "__class_"]
+        with self.assertRaises(AttributeError):
+            for k in unknown_keys:
+                bm.__getattribute__(k)
+
+    def test_check_type_of_id_from_dict(self):
+        bm = BaseModel()
+        temp = bm.to_dict()
+        self.assertIsInstance(temp["id"], str)
+
+    def test_check_type_of_created_at_from_dict(self):
+        bm = BaseModel()
+        temp = bm.to_dict()
+        self.assertIsInstance(temp["created_at"], str)
+
+    def test_check_type_of_updated_at_from_dict(self):
+        bm = BaseModel()
+        temp = bm.to_dict()
+        self.assertIsInstance(temp["updated_at"], str)
+
+    def test_check_type_of_class_name_from_dict(self):
+        bm = BaseModel()
+        temp = bm.to_dict()
+        self.assertIsInstance(temp["__class__"], str)
+
+
+class TestBaseModelStringRepresentationMethod(unittest.TestCase):
+    """Unittests for testing str magic method of the Base Model class"""
+
+    def test_str_method(self):
+        bm = BaseModel()
+        self.assertIn("BaseModel", str(bm))
+
+    def test_if_empty(self):
+        bm = BaseModel()
+        self.assertTrue(str(bm) != "" and str(bm) is not None)
+
+
+if __name__ == "__main__":
+    unittest.main()
