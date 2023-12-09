937e452e0fcf0f7dc54fbaed4669aa33a7fb684f
hamzahdili2001 <hdilihamza955@gmail.com>
2023-12-09 19:13:34 +0100

    [add] more test form the old ones

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -48,5 +48,93 @@ class TestBaseModelInstantiation(unittest.TestCase):
         self.assertNotEqual(first_dict["updated_at"], sec_dict["updated_at"])
 
 
+class TestBaseModelSaveMethod(unittest.TestCase):
+    """Unittests for testing save method of the Base Model class"""
+
+    def test_save_object(self):
+        bm = BaseModel()
+        temp = bm.updated_at
+        bm.save()
+        self.assertTrue(bm.updated_at > temp)
+
+
+class TestBaseModelToDictMethod(unittest.TestCase):
+    """Unittests for testing to_dict method of the Base Model class"""
+
+    def test_contain_known_keyword(self):
+        bm = BaseModel()
+        known_keys = ["id", "created_at", "updated_at", "__class__"]
+        for k in known_keys:
+            self.assertIsNotNone(bm.__getattribute__(k))
+
+    def test_contain_unknown_keyword(self):
+        bm = BaseModel()
+        unknown_keys = ["i", "created_a", "updated_a", "__class_"]
+        with self.assertRaises(AttributeError):
+            for k in unknown_keys:
+                bm.__getattribute__(k)
+
+    def test_check_type_of_id_from_dict(self):
+        bm = BaseModel()
+        temp = bm.to_dict()
+        self.assertIsInstance(temp["id"], str)
+
+    def test_check_type_of_created_at_from_dict(self):
+        bm = BaseModel()
+        temp = bm.to_dict()
+        self.assertIsInstance(temp["created_at"], str)
+
+    def test_check_type_of_updated_at_from_dict(self):
+        bm = BaseModel()
+        temp = bm.to_dict()
+        self.assertIsInstance(temp["updated_at"], str)
+
+    def test_check_type_of_class_name_from_dict(self):
+        bm = BaseModel()
+        temp = bm.to_dict()
+        self.assertIsInstance(temp["__class__"], str)
+
+    def test_length_of_the_dict(self):
+        self.assertEqual(len(str(BaseModel().to_dict())), 160)
+
+
+class TestBaseModelStringRepresentationMethod(unittest.TestCase):
+    """Unittests for testing str magic method of the Base Model class"""
+
+    def test_str_method(self):
+        bm = BaseModel()
+        self.assertIn("BaseModel", str(bm))
+
+    def test_if_empty(self):
+        bm = BaseModel()
+        self.assertTrue(str(bm) != "" and str(bm) is not None)
+
+    def test_length_of_the_text(self):
+        self.assertGreaterEqual(len(str(BaseModel())), 220)
+
+
+class TestBaseModelStorageClass(unittest.TestCase):
+    """Unittests for testing instance of FileStorage in the Base Model class"""
+
+    def test_storage_all_method_without_instantiation(self):
+        self.assertNotEqual(len(storage.all()), 0)
+
+    def test_storage_all_method_with_instantiation(self):
+        _ = BaseModel()
+        self.assertGreaterEqual(len(storage.all()), 1)
+
+    def test_storage_all_method_with_kwargs_instantiation(self):
+        _ = BaseModel(**({"name": "python"}))
+        self.assertGreaterEqual(len(storage.all()), 1)
+
+    def tearDown(self) -> None:
+        self.empty_file()
+
+    def empty_file(self):
+        filename = "objects.json"
+        with open(filename, "w", encoding="utf-8") as file:
+            print("{}", file=file, end="")
+
+
 if __name__ == "__main__":
     unittest.main()
