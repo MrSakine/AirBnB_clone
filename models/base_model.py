dea37d1546c2b72beb7c891f47f4089644eded31
MrSakine <msakine20@gmail.com>
2023-12-09 18:27:16 +0000

    [update] base model init method

--- models/base_model.py
+++ models/base_model.py
@@ -24,10 +24,11 @@ class BaseModel:
             - created_at (optional, datetime): the creation date of the object
             - updated_at (optional, datetime): last modified date of the object
         """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
         if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
             models.storage.new(self)
         else:
             time_format = "%Y-%m-%dT%H:%M:%S.%f"
